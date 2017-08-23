719f3e0b6aba9c39b70d70c0c4eac006a43a4ba7
ijlee2 <ijlee2@users.noreply.github.com>
2017-08-23 17:41:06 -0500

    isaac: initial builds for api demos

--- api_demos/assets/javascript/zomato.js
+++ api_demos/assets/javascript/zomato.js
@@ -1,5 +1,3 @@
-// Key: b5ee5402ab1c74adead9b0b432d1db0f
-
 // Find all bbq locations in Austin, TX
 var api_url = "https://developers.zomato.com/api/v2.1/search?q=bbq%2C%20barbecue&lat=30.307182&lon=-97.755996&radius=100000";
 
