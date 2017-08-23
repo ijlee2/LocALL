719ac8e4a5c8ac56858a0e1cb44f064758a4edbf
ijlee2 <ijlee2@users.noreply.github.com>
2017-08-23 16:27:00 -0500

    isaac: initial builds for api demos

--- api_demos/assets/javascript/brewerydb.js
+++ api_demos/assets/javascript/brewerydb.js
@@ -5,22 +5,21 @@
     
 *****************************************************************************
 *****************************************************************************/
-var api_url = "https://api.brewerydb.com/v2/?key=cc8daa1e9ba37ff2c611bfa432a7d04a&format=json";
+var api_url = "https://api.brewerydb.com/v2/?key=cc8daa1e9ba37ff2c611bfa432a7d04a";
 
-/*
+
 $.ajax({
     "url"   : api_url,
     "method": "GET"}
 
 ).done(function(response) {
     console.log(response);
+    
 });
-*/
 
+
+/* Alternate method
 $.getJSON(api_url, function(json) {
     console.log(json);
 });
-
-$(document).ready(function() {
-
-});
+*/
