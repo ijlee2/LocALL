ac15a8e24c8029a7b1c9d2c4b762db26dc4d68c1
ijlee2 <ijlee2@users.noreply.github.com>
2017-08-23 16:37:45 -0500

    isaac: initial builds for api demos

---
+++ api_demos/assets/javascript/beer_mapping.js
@@ -0,0 +1,6 @@
+// Find all beer locations in Austin, TX
+var api_url = "http://beermapping.com/webservice/loccity/68a06dbe893eb0da2b905eecb80e8c2f/austin,tx&s=json";
+
+$.getJSON(api_url, function(json) {
+    console.table(json);
+});
--- api_demos/assets/javascript/brewerydb.js
+++ api_demos/assets/javascript/brewerydb.js
@@ -1,22 +1,14 @@
-/****************************************************************************
- ****************************************************************************
-    
-    Configure API
-    
-*****************************************************************************
-*****************************************************************************/
+// Configure API    
 var api_url = "https://api.brewerydb.com/v2/?key=cc8daa1e9ba37ff2c611bfa432a7d04a";
-
 
 $.ajax({
     "url"   : api_url,
     "method": "GET"}
 
 ).done(function(response) {
-    console.log(response);
-    
-});
+    console.table(response);
 
+});
 
 /* Alternate method
 $.getJSON(api_url, function(json) {
