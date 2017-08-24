a1e4cca287119a3210d87b1a2951525f4ebc5859
ijlee2 <ijlee2@users.noreply.github.com>
2017-08-24 13:53:07 -0500

    isaac: api js updated

--- api_demos/assets/javascript/google_maps.js
+++ api_demos/assets/javascript/google_maps.js
@@ -1,11 +1,18 @@
 var map;
 var places = [{"name"      : "Rudy's BBQ",
+               "address"   : "512 W 29th Street, Austin 78705",
                "latitude"  : 30.2953166667,
                "longitude:": -97.7423694444},
                
               {"name"      : "Bert's BBQ",
+               "address"   : "907 W 24th Street, Austin 78705",
                "latitude"  : 30.2953166667,
-               "longitude:": -97.7423694444}
+               "longitude:": -97.7423694444},
+               
+              {"name"      : "BBQ Revolution",
+               "address"   : "3111 Manor Road, Austin 78723",
+               "latitude"  : 30.3154988,
+               "longitude:": -97.7167106}
              ];
 
 function displayMap() {
