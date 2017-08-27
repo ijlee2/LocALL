5bad065c371c04c78f9ed5ed9c4aa607e9f67034
ijlee2 <ijlee2@users.noreply.github.com>
2017-08-26 21:41:31 -0500

    initial build

--- api_demos/assets/js/authenticate_users.js
+++ api_demos/assets/js/authenticate_users.js
@@ -1,17 +1,49 @@
-$("#button_signup").on("click", function() {
+let pageStatus = "signup";
+
+$("#button_signup").click(function() {
+    pageStatus = "signup";
+
     $("#header").text("Sign Up");
     $("#userName_container").css({"display": "block"});
 
 });
 
-$("#button_login").on("click", function() {
+$("#button_login").click(function() {
+    pageStatus = "login";
+
     $("#header").text("Log In");
     $("#userName_container").css({"display": "none"});
 
 });
 
-$("#button_logout").on("click", function() {
+$("#button_logout").click(function() {
+    pageStatus = "logout";
+
     $("#header").text("Log Out");
     $("#userName_container").css({"display": "none"});
     
+});
+
+
+$("#button_submit").click(function() {
+    // TODO - Jacque: Validate first name (must be all letters)
+    const name = $("#userName").val().trim();
+
+    // TODO - Jacque: Validate email (must have format of ***@***.com)
+    const email = $("#userEmail").val().trim();
+
+    // TODO - Jacque: Validate password (8-64 characters, 1 letter, 1 number, 1 special character)
+    const password = $("#userPassword").val();
+
+
+    if (pageStatus === "signup") {
+        // TODO - Isaac: Create an account on Firebase
+
+    } else if (pageStatus === "login") {
+        // TODO - Isaac: Log in to an existing account
+
+    } else if (pageStatus === "logout") {
+        // TODO - Isaac: Log out of an existing account
+
+    }
 });
