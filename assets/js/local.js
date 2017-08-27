6ab421fa80bd016dac774146baaa705fdd46f432
taggie05 <tagaban2005@yahoo.com>
2017-08-27 00:36:40 -0500

    add

--- assets/js/local.js
+++ assets/js/local.js
@@ -1,6 +1,7 @@
-$('.dropdown-toggle').dropdown("toggle");
+$('.dropdown-toggle').dropdown("update");
 function displayPage(page){
 	$(".page").hide();
 	$(".page:nth-of-type(" + (page + 1) + ")").show();
-}
+};
+
 
