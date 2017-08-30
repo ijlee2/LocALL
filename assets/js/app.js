f1de132bce38f1be019a4767a21bb24a610eef1a
taggie05 <tagaban2005@yahoo.com>
2017-08-30 18:17:13 -0500

    added more functionality to slides

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,20 @@
+$(function() {
+    /* Initialize Carousel */
+    var paused = 0;
+    $('#myCarousel').carousel({
+      interval: 1000,
+      pause: 0
+    });
+  
+    /* Play trigger */
+    $('#toggleCarousel').click(function() {
+      var state = (paused) ? 'cycle' : 'pause';
+      paused = (paused) ? 0 : 1;
+      $('#myCarousel').carousel(state);
+      $(this).find('i').toggleClass('fa-play fa-pause');
+    });
+  });
+  
 /****************************************************************************
  ****************************************************************************
     
